fc937b997f3d6b05cad9b996188a68f04212c531
serhiyk <serhiy.krasnoshchok@gmail.com>
2016-05-17 21:44:49 +0300

    Added blocking SPI transfer function.

--- src/drivers/SPI.c
+++ src/drivers/SPI.c
@@ -60,6 +60,18 @@ uint8_t spi_master_transfer(uint8_t *tx_buf, uint8_t *rx_buf, uint8_t len, spi_c
     return 0;
 }
 
+void spi_master_transfer_blocking(uint8_t *tx_buf, uint8_t *rx_buf, uint8_t len)
+{
+    while (spi_descriptor.state != SPI_IDLE);
+    spi_descriptor.tx_buf = tx_buf + 1;
+    spi_descriptor.rx_buf = rx_buf;
+    spi_descriptor.len = len;
+    spi_descriptor.callback = 0;
+    spi_descriptor.state = SPI_MASTER_TX;
+    SPDR = *tx_buf;
+    while (spi_descriptor.state != SPI_IDLE);
+}
+
 uint8_t spi_ready(void)
 {
     return spi_descriptor.state == SPI_IDLE;
--- src/drivers/SPI.h
+++ src/drivers/SPI.h
@@ -6,8 +6,10 @@
 typedef void (*spi_cb)(void);
 void spi_master_init(void);
 uint8_t spi_master_transfer(uint8_t *tx_buf, uint8_t *rx_buf, uint8_t len, spi_cb callback);
+void spi_master_transfer_blocking(uint8_t *tx_buf, uint8_t *rx_buf, uint8_t len);
 uint8_t spi_ready(void);
 
 #define spi_master_send(tx_buf,len,callback) spi_master_transfer(tx_buf,0,len,callback)
+#define spi_master_send_blocking(tx_buf,len) spi_master_transfer_blocking(tx_buf,0,len)
 
 #endif /* SPI_H */
