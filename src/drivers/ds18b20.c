f38a858b7425817f6a710c959440f7fc84c57059
serhiyk <serhiy.krasnoshchok@gmail.com>
2016-06-12 21:49:00 +0300

    Added DS18B20 driver

---
+++ src/drivers/ds18b20.c
@@ -0,0 +1,109 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include "onewire.h"
+#include "ds18b20.h"
+
+#define CMD_SKIP_ROM 0xCC
+#define CMD_CONVERT_T 0x44
+#define CMD_READ_SCRATCHPAD 0xBE
+
+#define DS18B20_READY 0
+#define DS18B20_BUSY 1
+
+static uint8_t state=DS18B20_READY;
+static ds18b20_cb ds18b20_callback;
+static uint16_t temperature;
+
+void _convert_t_cb(uint8_t data)
+{
+    state = DS18B20_READY;
+}
+
+void _convert_skip_rom_cb(uint8_t data)
+{
+    onewire_master_write(CMD_CONVERT_T, _convert_t_cb);
+}
+
+void _pull_up2_cb(uint8_t data)
+{
+    if (data)
+    {
+        state = DS18B20_READY;
+        return;
+    }
+    onewire_master_write(CMD_SKIP_ROM, _convert_skip_rom_cb);
+}
+
+void _pull_down2_cb(uint8_t data)
+{
+    onewire_master_pull_up(_pull_up2_cb);
+}
+
+uint8_t ds18b20_convert(void)
+{
+    if (state != DS18B20_READY)
+    {
+        return 1;
+    }
+    state = DS18B20_BUSY;
+    onewire_master_pull_down(_pull_down2_cb);
+    return 0;
+}
+
+void _read_temperature_2_cb(uint8_t data)
+{
+    temperature |= data << 8;
+    if (ds18b20_callback)
+    {
+        ds18b20_callback(temperature);
+    }
+    state = DS18B20_READY;
+}
+
+void _read_temperature_1_cb(uint8_t data)
+{
+    temperature = data;
+    onewire_master_read(_read_temperature_2_cb);
+}
+
+void _read_scratchpad_cb(uint8_t data)
+{
+    onewire_master_read(_read_temperature_1_cb);
+}
+
+void _read_skip_rom_cb(uint8_t data)
+{
+    onewire_master_write(CMD_READ_SCRATCHPAD, _read_scratchpad_cb);
+}
+
+void _pull_up1_cb(uint8_t data)
+{
+    if (data)
+    {
+        if (ds18b20_callback)
+        {
+            ds18b20_callback(data);
+        }
+        state = DS18B20_READY;
+        return;
+    }
+    onewire_master_write(CMD_SKIP_ROM, _read_skip_rom_cb);
+}
+
+void _pull_down1_cb(uint8_t data)
+{
+    onewire_master_pull_up(_pull_up1_cb);
+}
+
+uint8_t ds18b20_read_temperature(ds18b20_cb callback)
+{
+    if (state != DS18B20_READY)
+    {
+        return 1;
+    }
+    state = DS18B20_BUSY;
+    ds18b20_callback = callback;
+    onewire_master_pull_down(_pull_down1_cb);
+    return 0;
+}
---
+++ src/drivers/ds18b20.h
@@ -0,0 +1,9 @@
+#ifndef DS18B20_H
+#define DS18B20_H
+
+typedef void (*ds18b20_cb)(uint16_t data);
+
+uint8_t ds18b20_convert(void);
+uint8_t ds18b20_read_temperature(ds18b20_cb callback);
+
+#endif  /* DS18B20_H */
