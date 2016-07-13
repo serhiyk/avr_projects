ef95e2870fb33ddc6a33f52aae5c150e2fa2ee8f
serhiyk <serhiy.krasnoshchok@gmail.com>
2016-07-13 20:57:45 +0300

    Added sensor application

---
+++ src/sensor/config.h
@@ -0,0 +1,33 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* nRF24L01 config */
+// The RF channel frequency: F = 2400 + CONFIG_NRF_CHANNEL [MHz] (0 - 125)
+#define CONFIG_NRF_CHANNEL 124
+// RF output power in TX mode (0 - 3)
+#define CONFIG_NRF_POWER 3
+// Air Data Rate (0 - 1Mbps; 1 - 2Mbps)
+#define CONFIG_NRF_DATA_RATE 0
+// Auto Retransmit Delay (0 - 250uS ... 15 - 4000uS)
+#define CONFIG_NRF_AUTO_RETRANSMIT_DELAY 1
+// Auto Retransmit Count (0 - 15)
+#define CONFIG_NRF_AUTO_RETRANSMIT_COUNT 4
+// #define CONFIG_NRF_ENABLE_RX
+#define CONFIG_NRF_ENABLE_AUTO_ACKNOWLEDGMENT
+#define NRF_CSN_PORT PORTD
+#define NRF_CSN_DDR DDRD
+#define NRF_CSN_PIN PD5
+#define NRF_CE_PORT PORTD
+#define NRF_CE_DDR DDRD
+#define NRF_CE_PIN PD4
+// Max RX message length (1 - 32)
+#define CONFIG_NRF_RX_MAX_LEN 32
+#define CONFIG_NRF_RX_PIPE_NUMBER 2
+
+/* 1wire */
+#define ONEWIRE_PORT PORTB
+#define ONEWIRE_DDR DDRB
+#define ONEWIRE_PORT_IN PINB
+#define ONEWIRE_PIN PB0
+
+#endif  /* CONFIG_H */
---
+++ src/sensor/main.c
@@ -0,0 +1,82 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+#include <util/delay.h>
+#include "UART.h"
+#include "timer.h"
+#include <stdint.h>
+#include "serial.h"
+#include "ds18b20.h"
+#include "nrf24l01.h"
+#include "onewire.h"
+
+#define ONEWIRE_VDD_PORT PORTB
+#define ONEWIRE_VDD_DDR DDRB
+#define ONEWIRE_VDD_PIN PB1
+#define ONEWIRE_GND_PORT PORTD
+#define ONEWIRE_GND_DDR DDRD
+#define ONEWIRE_GND_PIN PD7
+
+uint8_t tx_address[5] = {0xD7,0xA1,0x17,0x0A,0xD7};
+static uint8_t tx_buf[5];
+
+void convert_temperature(void);
+
+void setup(void)
+{
+    ONEWIRE_VDD_DDR = 1 << ONEWIRE_VDD_PIN;
+    ONEWIRE_VDD_PORT = 1 << ONEWIRE_VDD_PIN;
+    ONEWIRE_GND_DDR |= 1 << ONEWIRE_GND_PIN;
+    ONEWIRE_GND_PORT &= ~(1 << ONEWIRE_GND_PIN);
+    spi_master_init();
+    serial_init();
+    timer_init();
+    onewire_init();
+    asm("sei");
+    //_delay_ms(5);
+    nrf24l01_init();
+    nrf24_set_tx_addr(tx_address, sizeof(tx_address));
+    set_sleep_mode(SLEEP_MODE_IDLE);
+}
+
+void temperature_cb(uint16_t data)
+{
+    tx_buf[0] = data >> 8;
+    tx_buf[1] = data & 0xFF;
+    nrf24_send(tx_buf, sizeof(tx_buf));
+    if (data & 0x0800)
+    {
+        data = ~data;
+        data++;
+        data |= 0x0800;
+    }
+    uart_send_hex(data>>4);
+    uart_send_hex(data&0x0F);
+}
+
+void read_temperature(void)
+{
+    ds18b20_read_temperature(temperature_cb);
+    timer_register(0, 10, convert_temperature);
+}
+
+void convert_temperature(void)
+{
+    ds18b20_convert();
+    timer_register(0, 10, read_temperature);
+}
+
+int main(void)
+{
+    setup();
+    // timer_register(0, 10, convert_temperature);
+    convert_temperature();
+    uart_send_byte('*');
+    while(1)
+    {
+        serial_handler();
+        nrf24_handler();
+        timer_handler();
+        sleep_mode();
+    }
+}
