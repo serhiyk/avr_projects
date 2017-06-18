693b95837ec0c2cf01faf1e8b3c07ff71b4abd62
serhiyk <serhiy.krasnoshchok@gmail.com>
2017-06-18 16:02:37 +0300

    Updated serial protocol.

--- src/scripts/set_time.py
+++ src/scripts/set_time.py
@@ -4,13 +4,33 @@ import time
 import datetime
 import serial
 
-SERIAL_COMMAND_START = '0'
+START_CODE = 0x30
+END_CODE = 0x20
+ESC_CODE = 0xF0
+START_REP_CODE = 0xF1
+END_REP_CODE = 0xF2
+ESC_REP_CODE = 0xF3
+
 COMMAND_ID_SET_TIME = '\x00'
-SERIAL_COMMAND_END = ' '
+
+
+def pack_data(data):
+    _data = chr(START_CODE)
+    for c in data:
+        if ord(c) == START_CODE:
+            c = chr(ESC_CODE) + chr(START_REP_CODE)
+        elif ord(c) == END_CODE:
+            c = chr(ESC_CODE) + chr(END_REP_CODE)
+        elif ord(c) == ESC_CODE:
+            c = chr(ESC_CODE) + chr(ESC_REP_CODE)
+        _data += c
+    _data += chr(END_CODE)
+    return _data
 
 
 def serial_send(data):
     ser = serial.Serial(port='/dev/ttyUSB0', baudrate=57600)
+    data = pack_data(data)
     ser.write(data)
     ser.close()
 
@@ -30,7 +50,7 @@ def set_time(year, month, day, hour, minute, second):
     if week_day == 8:
         week_day = 1
     cur_time = '{:02d}{:02d}{:02d}{:02d}{:02d}{:02d}{:02d}'.format(second, minute, hour, week_day, day, month, year)
-    data = SERIAL_COMMAND_START + COMMAND_ID_SET_TIME + cur_time + SERIAL_COMMAND_END
+    data = COMMAND_ID_SET_TIME + cur_time
     serial_send(data)
 
 
@@ -39,7 +59,7 @@ def set_cur_time():
     cur_time = list(cur_time)
     cur_time[7] = chr(ord(cur_time[7]) + 1)  # correct week day
     cur_time = ''.join(cur_time)
-    data = COMMAND_ID_SET_TIME + cur_time + SERIAL_COMMAND_END
+    data = COMMAND_ID_SET_TIME + cur_time
     serial_send(data)
 
 
